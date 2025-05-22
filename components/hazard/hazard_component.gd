2eef2e4aee3e3f8a9b324d58789067dcbdac1665
SvZ <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-05-22 21:36:54 +0200

    Add hazard component

---
+++ components/hazard/hazard_component.gd
@@ -0,0 +1,9 @@
+class_name HazardComponent
+extends Area2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.die()
